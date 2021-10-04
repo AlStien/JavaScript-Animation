9aa1d064e1da7c8d8a66e096af08e9710cfc9eb4
AlStien <aj601604@gmail.com>
2021-10-05 00:22:25 +0530

    minor changes in layout and added layer effect

--- JavaScripts/script.js
+++ JavaScripts/script.js
@@ -1,39 +1,46 @@
-let bg = ['url(Images/Morning.jpg)', 'url(Images/Desert.jpg)', 'url(Images/Fuji.jpg)', 'url(Images/Desert-2.jpg)'];
+let bg = ['url(../Images/Morning.jpg)', 'url(../Images/Desert.jpg)', 'url(../Images/Fuji.jpg)', 'url(../Images/Desert-2.jpg)'];
+let bg_obj = ['url(../Images/Morning-tree.png)', 'url(../Images/desert-cactus.png)', 'url(../Images/fuji-tree.png)','url(../Images/empty.png)']
 let objArr = ['url(Images/naruto-run-1.png)', 'url(Images/naruto-run-2.png)', 'url(Images/naruto-run-3.png)', 'url(Images/naruto-run-4.png)']
+let bgr = document.getElementById("bground");
+let bgr_obj = document.getElementById("bground_object");
 let i=0, j=0;
 function nextBG()
 {
     i++;
     if(i===4)
         i=0;
-    document.getElementById("bground").style.backgroundImage = bg[i];
+    bgr.style.backgroundImage = bg[i];
+    bgr_obj.style.backgroundImage = bg_obj[i];
 }
 
 function prevBG()
 {
     i--;
     if(i===-1)
+    {
         i=3;
-    document.getElementById("bground").style.backgroundImage = bg[i];
+    }
+    bgr.style.backgroundImage = bg[i];
+    bgr_obj.style.backgroundImage = bg_obj[i];
 }
 
 function Move()
 {
     //background move
     let id=null;
-    let bgr = document.getElementById("bground");
     let pos = 0;
     clearInterval(id);
     id = setInterval(bgrMove, 10);
     function bgrMove()
     {
-        pos--;
+        pos=pos-3;
+        bgr_obj.style.backgroundPositionX = pos + "px";
         bgr.style.backgroundPositionX = pos + "px";
     }
 
     //object move
     let obj = document.getElementById("object");
-    let ob = setInterval(objMove, 250);
+    let ob = setInterval(objMove, 120);
     function objMove()
     {
         j++;
