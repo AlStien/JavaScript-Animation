ce084c2c1e522e647724bfc974b1944456fdc289
AlStien <aj601604@gmail.com>
2021-10-04 19:45:48 +0530

    added running character

--- JavaScripts/script.js
+++ JavaScripts/script.js
@@ -1,5 +1,6 @@
 let bg = ['url(Images/Morning.jpg)', 'url(Images/Desert.jpg)', 'url(Images/Fuji.jpg)', 'url(Images/Desert-2.jpg)'];
-let i=0;
+let objArr = ['url(Images/naruto-run-1.png)', 'url(Images/naruto-run-2.png)', 'url(Images/naruto-run-3.png)', 'url(Images/naruto-run-4.png)']
+let i=0, j=0;
 function nextBG()
 {
     i++;
@@ -16,17 +17,28 @@ function prevBG()
     document.getElementById("bground").style.backgroundImage = bg[i];
 }
 
-let id=null;
-function bgMove()
+function Move()
 {
+    //background move
+    let id=null;
     let bgr = document.getElementById("bground");
     let pos = 0;
     clearInterval(id);
     id = setInterval(bgrMove, 10);
     function bgrMove()
     {
-        pos++;
+        pos--;
         bgr.style.backgroundPositionX = pos + "px";
     }
 
+    //object move
+    let obj = document.getElementById("object");
+    let ob = setInterval(objMove, 250);
+    function objMove()
+    {
+        j++;
+        if(j===4)
+            j=0;
+        obj.style.backgroundImage = objArr[j];
+    }
 }
