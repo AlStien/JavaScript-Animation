eda65b43447a0b7cc6ec2b433d8a8287d5e6cdcf
AlStien <aj601604@gmail.com>
2021-10-05 22:18:58 +0530

    fixed mountain background and added comments

--- JavaScripts/script.js
+++ JavaScripts/script.js
@@ -1,3 +1,4 @@
+//accessing objects from main HTML document
 let bg = ['url(Images/Morning.jpg)', 'url(Images/Desert.jpg)', 'url(Images/Fuji.jpg)', 'url(Images/Desert-2.jpg)'];
 let bg_obj = ['url(Images/Morning-tree.png)', 'url(Images/desert-cactus.png)', 'url(Images/fuji-tree.png)','url(Images/empty.png)']
 let objArr = ['url(Images/naruto-run-1.png)', 'url(Images/naruto-run-2.png)', 'url(Images/naruto-run-3.png)', 'url(Images/naruto-run-4.png)']
@@ -13,8 +14,8 @@ function nextBG()
     i++;
     if(i===4)
         i=0;
-    bgr.style.backgroundImage = bg[i];
-    bgr_obj.style.backgroundImage = bg_obj[i];
+    bgr.style.backgroundImage = bg[i];      //accessing next background from background array
+    bgr_obj.style.backgroundImage = bg_obj[i];      //the tree, cactus to be displayed over the charactter to give behind look
 }
 
 //to bring previous background
@@ -25,26 +26,25 @@ function prevBG()
     {
         i=3;
     }
-    bgr.style.backgroundImage = bg[i];
-    bgr_obj.style.backgroundImage = bg_obj[i];
+    bgr.style.backgroundImage = bg[i];      //accessing previous background from background array
+    bgr_obj.style.backgroundImage = bg_obj[i];      //the tree, cactus to be displayed over the charactter to give behind look
 }
 
 //for moving the background, object and object animation
 function Move()
 {
     //background move
-    let bgPos = 0;
+    let bgPos = 0;      //variable to give background position
     setInterval(bgrMove, 10);
     function bgrMove()
     {
-        if(b === false)
-        {
+        if(b === false)         //to move background forward as object moves backward
             bgPos=bgPos-3;
-        }
-        else
+        else         //to move background backward as object moves forward
             bgPos=bgPos+3;
-        bgr_obj.style.backgroundPositionX = bgPos + "px";
-        bgr.style.backgroundPositionX = bgPos + "px";
+
+        bgr_obj.style.backgroundPositionX = bgPos + "px";       //to move background
+        bgr.style.backgroundPositionX = bgPos + "px";       //the move tree, cactus overlay
     }
 
     //object running animation
@@ -54,31 +54,35 @@ function Move()
         j++;
         if(j===4)
             j=0;
-        obj.style.backgroundImage = objArr[j];
+        obj.style.backgroundImage = objArr[j];      //changing object image to give running look
     }
 
     //object movement
-    let objPos = 60;
+    let objPos = 60;        //to make initial position of object
     setInterval(objMove, 10);
     function objMove()
     {
-        if(b===false)
+        if(b===false)       //to move object forward
             objPos = objPos+1.5;
-        else
+        else        //to move object backwards from other end
             objPos = objPos-1.5
+
         obj.style.left = objPos + "px";
-        //changing object direction when reaching end of background
+
+        //to change object direction when reaching right end
         if(parseInt(object.style.left) >= screen.width*8.05/10)
         {
-            obj.style.transform = "rotateY(180deg)";
             b = true;
+            obj.style.transform = "rotateY(180deg)";        //to bring object in reverse direction
             nextBG();       //to change background automatically whenever object changes direction
         }
+
+        //to change object direction when reaching left end
         if(parseInt(object.style.left) <= -80)
         {
-            obj.style.transform = "rotateY(360deg)";
             b = false;
-            nextBG();
+            obj.style.transform = "rotateY(360deg)";        //to bring object in its original direction
+            nextBG();       //to change background automatically whenever object changes direction
         }
     }
 }
